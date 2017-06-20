4fb5bc94ebbc826e7e3c6819beed9218ca27371e
Lannt <nguyenthilanuet96@gmail.com>
2017-06-20 18:34:42 +0700

    create

---
+++ src/Application.java
@@ -0,0 +1,8 @@
+/**
+ * Created by nguye on 6/20/2017.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello word !");
+    }
+}
---
+++ src/test/Student.java
@@ -0,0 +1,25 @@
+package test;
+
+/**
+ * Created by nguye on 6/20/2017.
+ */
+public class Student {
+    int age;
+    String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
